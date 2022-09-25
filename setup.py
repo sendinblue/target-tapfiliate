633bedee413438caa09b71da120166dbeb2a2b40
bbaltagi-dtsl <bilal.baltagi@sendinblue.com>
2022-09-25 18:39:00 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="target-tapfiliate",
+    version="0.1.0",
+    description="Singer.io target for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["target_tapfiliate"],
+    install_requires=[
+        "singer-python>=5.0.12",
+    ],
+    entry_points="""
+    [console_scripts]
+    target-tapfiliate=target_tapfiliate:main
+    """,
+    packages=["target_tapfiliate"],
+    package_data = {},
+    include_package_data=True,
+)
